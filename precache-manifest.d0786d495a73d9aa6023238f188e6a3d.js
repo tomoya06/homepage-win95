49f008723b816f8c68f9a2b41d4ded8a31f0f154
tomoya06 <peng4766@gmail.com>
2019-10-21 00:40:35 +0800

    Updates

---
+++ precache-manifest.d0786d495a73d9aa6023238f188e6a3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d920c1706f09cd1b915c7056335c9e4d",
+    "url": "/homepage-win95/index.html"
+  },
+  {
+    "revision": "9b10de84cc5dbb2f06d0",
+    "url": "/homepage-win95/static/css/2.1b53b9c3.chunk.css"
+  },
+  {
+    "revision": "e360ab9fcc59125eb53c",
+    "url": "/homepage-win95/static/css/main.75dd32fa.chunk.css"
+  },
+  {
+    "revision": "9b10de84cc5dbb2f06d0",
+    "url": "/homepage-win95/static/js/2.6e87a835.chunk.js"
+  },
+  {
+    "revision": "e360ab9fcc59125eb53c",
+    "url": "/homepage-win95/static/js/main.a921aec0.chunk.js"
+  },
+  {
+    "revision": "4dbd135f874d22b04ef0",
+    "url": "/homepage-win95/static/js/runtime-main.a321539d.js"
+  }
+]);
